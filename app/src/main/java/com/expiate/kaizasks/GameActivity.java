def6e164583df320063556929dca2684926d438f
Expiate <Tuetano32@gmail.com>
2020-11-10 23:26:13 +0100

    QuestionsFragment is now Dynamically Embedded. Added a resource for the Answer Buttons on the QuestionsFragment.

--- app/src/main/java/com/expiate/kaizasks/GameActivity.java
+++ app/src/main/java/com/expiate/kaizasks/GameActivity.java
@@ -1,14 +1,30 @@
 package com.expiate.kaizasks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.expiate.kaizasks.fragments.QuestionsFragment;
+
 public class GameActivity extends AppCompatActivity {
+
+    private QuestionsFragment questionsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        questionsFragmentSetup();
+    }
+
+
+    public void questionsFragmentSetup() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        questionsFragment = QuestionsFragment.newInstance("test", "test");
+        ft.replace(R.id.placeholder, questionsFragment);
+        ft.commit();
     }
 }
